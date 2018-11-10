f127e8c8427637769b2c56cb1b074f39ada24f65
taiyo3 <taiyoyodai@gmail.com>
2018-11-10 19:18:25 +0900

    not test yet, so more refuctoring may be needed

--- FPU/fadd.sv
+++ FPU/fadd.sv
@@ -4,8 +4,7 @@
 module fadd(
   input wire [31:0] x1,
   input wire [31:0] x2,
-  output wire [31:0] y,
-  output wire ovf);
+  output wire [31:0] y);
   
   wire s1;
   wire s2;
@@ -15,80 +14,89 @@ module fadd(
   wire [22:0] m2;
      
   assign {s1, e1, m1}=x1;
-  assign {s2, e2, m2}=x2;
+  assign {s2, e2, m2}=x2; 
   
+  //x1の指数部が0の場合。そのときは、我々の仕様だとx1==0とみなすため、最後、y=x2を出力
+  wire state1;
+  assign state1 = (e1==8'b0) ? 1 : 0 ;
+
+  //x2の指数部が0の場合。そのときは、我々の仕様だとx2==0とみなすため、最後、y=x1を出力  
+  wire state2;
+  assign state2 = (e2==8'b0) ? 1 : 0 ;
+
   wire [24:0] m1a;
-  wire [24:0] m2a;  
+  wire [24:0] m2a; 
+  assign m1a= {2'b01, m1};  
+  assign m2a= {2'b01, m2};  
+   
+  wire [7:0] e2i;    
+  assign e2_inv=~e2;  //bit反転
 
-  assign m1a= (e1==0) ? 25'b0 : {2'b01, m1};  //ayasii
-  assign m2a= (e2==0) ? 25'b0 : {2'b01, m2}; 
-  
-  wire [7:0] e1a;
-  wire [7:0] e2a;  
 
-  assign e1a= (e1==8'b0) ? 1 : e1; //ayasii
-  assign e2a= (e2==8'b0) ? 1 : e2;  
- 
-  wire [7:0] e2ai;    
-  assign e2ai=~e2a;  //ayasii
+  //引き算の代わりに、反転bitして和をとり、2つの差を求める.  
+  wire [8:0] te; 
+  assign te={1'b0, e1}+{1'b0, e2_inv};        
+
   
-  wire [8:0] te; //nannbittoshiftsuruka
-  assign te={1'b0, e1a}+{1'b0, e2ai};        
-  
+  //ce==1だと、e1>e2を意味し、ce==0だとe1<=e2を意味する.flag.
   wire ce;
-  wire [7:0] tde;
-  assign ce=~te[8];  //ayasii
+  assign ce=te[8];  
   
-  wire [8:0] te2;
-  assign te2=te+9'b01;
-  wire [8:0] te3;
-  assign te3=~te;
-  assign tde = (ce==0) ? te2[7:0] : te3[7:0]; //ayasii
-
+  //[7:0]が,指数の差の絶対値.仮数部をずらして和を取るときに使用される
+  wire [8:0] tde;
+  assign tde = ce ? {te + 9'd1} : ~te; 
+  
+  //指数部の差が31以上あるときは、31を上限とした.
   wire [4:0] de;  
-  assign de = tde>31 ? 31 : tde[4:0];    //ayasii
+  assign de = (tde[7:0] > 8'd31) ? 31 : tde[4:0] ;
   
-  wire sel; //sel=0 means |x1|>|x2|. sel1=1 means |x1|<=|x2|
-  assign sel = (de==0) ? ((m1a > m2a) ? 0 : 1) : ce;  
+  //sel=0 means |x1|>|x2|. sel1=1 means |x1|<=|x2|  
+  wire sel; 
+  assign sel = (de==0) ? ((m1a > m2a) ? 0 : 1) : ~ce; 
   
+  
+  //追加した.|x1|==|x2|を意味する.最後に使う.
+  wire same_abs;
+  assign same_abs = ((de==0) && (m1a==m2a)) ? 1 : 0;
+
+
+  //s means superior, i means inferior  
   wire [24:0] ms;
   wire [24:0] mi;   
   wire [7:0] es;
-  wire [7:0] ei;  
   wire  ss;
   
   assign ms = (sel==0) ? m1a : m2a ;
   assign mi = (sel==0) ? m2a : m1a ;
-  assign es = (sel==0) ? e1a : e2a ;
-  assign ei = (sel==0) ? e2a : e1a ;
-  assign ss = (sel==0) ? s1  : s2  ;
+  assign es = (sel==0) ? e1 : e2 ;
+  assign ss = (sel==0) ? s1 : s2 ;
+
+
 
   wire [55:0] mie;
   assign mie={mi, 31'b0};
 
+  //inferiorの仮数部を、指数差分、ずらす.
   wire [55:0] mia;  
-  assign mia=mie>>de; //ayasii
+  assign mia=mie>>de; 
+
+
   
-  wire tstck;
-  assign tstck=|(mia[28:0]);
-  
-  
-  wire [26:0] mye; //if signs are same then add, and if they are different then sub
+  //if signs are same then add, and if they are different then sub  
+  //ずらして引いてるので、引き算問題ない.
+  wire [26:0] mye; 
   assign mye = (s1==s2) ? {ms, 2'b0}+mia[55:29]  : {ms, 2'b0} - mia[55:29] ;  
 
-  wire [7:0] esi;
-  assign esi=es+1;
-  
+
   wire [7:0] eyd;    
   wire [26:0] myd;
-  wire stck;    
   
-  wire flag1;
-  assign flag1 = (mye[26]==1 && esi==255) ? 1 : 0; //1 if overflow
-  assign myd = (mye[26]==0) ? mye  :  ( (esi==255) ?  {2'b01,25'b0} : mye>>1 )  ;  
-  assign eyd = (mye[26]==0) ? es   :  ( (esi==255) ?  255 : esi ) ;  
-  assign stck = (mye[26]==0) ? tstck :  tstck || mye[0] ;      
+  //基本的にはmye[26]==0である.  
+  assign myd = (mye[26]==0) ? mye  :  (mye>>1)  ;  
+  assign eyd = (mye[26]==0) ? es   :  es+8'd1 ;  
 
+
+  //どこに初めて1が立っているか調べる
   wire [4:0] se;    
   assign se = (myd[25] == 1) ? 0 :
               (myd[24] == 1) ? 1 :
@@ -116,71 +124,26 @@ module fadd(
               (myd[2] == 1) ? 23 :
               (myd[1] == 1) ? 24 :
               (myd[0] == 1) ? 25 :  26;
-              
+
+  //基本的に、出力すべき指数部          
   wire [8:0] eyf;      
   assign eyf = {1'b0, eyd} - {4'b0, se};
-
-  wire [7:0] eyr;    
+  
+  //ただし、seの方が大きい時は、非正規化数とする.
+  wire [7:0] ey;    
+  assign ey = ({1'b0, eyd} > {4'b0, se}) ? eyf[7:0] : 8'b0 ;                                
+  
+  //我々の仕様では、非正規化数になるときは、仮数部27桁全て0にしてもよい
   wire [26:0] myf;
-  assign myf = ({1'b0, eyd} > {4'b0, se}) ? (myd<<se)  : myd<<(eyd[4:0]-1) ;                
-  assign eyr = ({1'b0, eyd} > {4'b0, se}) ? eyf[7:0] : 8'b0  ;                              
+  assign myf = ({1'b0, eyd} > {4'b0, se}) ? (myd<<se) : 27'd0 ;           
   
-  wire a;
-  assign a=(myf[1] == 1 && myf[0] == 0 && stck == 0 && myf[2] == 1) ? 1 : 0;
-  wire b;
-  assign b=(myf[1] == 1 && myf[0] == 0 && s1 == s2 && stck ==1) ? 1 : 0;
-  wire c;
-  assign c=(myf[1] == 1 && myf[0] == 1) ? 1 : 0 ;
-  wire d;
-  assign d=a||b||c ? 1 : 0;
-
-
-
-  wire [24:0] myr;  
-  assign myr = (d==1) ? (myf[26:2] + 23'b1) : myf[26:2];
-  
-  wire [7:0] eyri;      
-  assign eyri = eyr + 8'b1;
-  
-  wire [7:0] ey;        
-  wire [22:0] my;       
-  wire fack;
-  assign fack = |(myr[23:0]) ; //1 if at least there is 1. 0 if all of them is 0 
-  assign ey = (myr[24]==1) ? eyri  : ((fack==0) ? 0 : eyr) ; //overflow arier.  honnmani 0 ka? 7'b0では?               
-  assign my = (myr[24]==1) ? 23'b0 : ((fack==0) ? 23'b0 : myr[22:0])  ;      
-  
-  wire flag2;
-  assign flag2 = ((myr[24]==1 && eyri==255) || flag1==1) ? 1 : 0; //1 if overflow
+  wire [22:0] my;
+  assign my = myf[24:2] ;     
+ 
   wire sy;
-  assign sy = (ey==0 && my==0) ? s1&&s2 : ss;
-  
-  wire nzm1;
-  wire nzm2;
-  assign nmz1 = |(m1[22:0]); 
-  assign nmz2 = |(m2[22:0]);              
-  
-  assign y = 
-    (e1 == 255 && e2 != 255) ? {s1, 8'd255, nmz1, m1[21:0]} :
-    (e2 == 255 && e1 != 255) ? {s2, 8'd255, nmz2, m2[21:0]} :
-    (e1 == 255 && e2 == 255 && nmz2) ? {s2, 8'd255, 1'b1, m2[21:0]} :
-    (e1 == 255 && e2 == 255 && nmz1) ? {s1, 8'd255, 1'b1, m1[21:0]} :
-    (e1 == 255 && e2 == 255 && s1==s2) ? {s1, 8'd255, 23'b0} :
-    (e1 == 255 && e2 == 255) ? {1'b1, 8'd255, 1'b1, 22'b0} :   //+mugenn-mugenn=NaN. sentou 1'b0deha?
-    {sy,ey,my};    
-    
-  //overflow ha wakaran
-  assign ovf=(e1<255 && e2<255 && flag2==1) ? 1 : 0;
+  assign sy = same_abs ? (s1 && s2) : ss;
+         
+  assign y = state1 ? x2 : (state2 ? x1 : {sy,ey,my}) ;
   
 endmodule   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
    
