c5e2cfe59f24d81811d5197ba101f9f0c6bef4cf
momohatt <momohatt10@gmail.com>
2018-11-13 01:32:26 +0900

    impl atan

---
+++ asm/libtest/atan.s
@@ -0,0 +1,24 @@
+main:
+	addi	sp,sp,-8
+	sw	ra,4(sp)
+	sw	fp,0(sp)
+	addi	fp,sp,8     # fp: old stack pointer
+#	main program starts
+#	fli	fa0, L_0	# 0.0
+#	fli	fa0, L_A5	# atan(0.2) = 0.19739555984988078
+#	fli	fa0, L_1	# 0.7853981633974483
+#	fli	fa0, L_2	# 1.1071487177940906
+	fli	fa0, L_2PI	# 1.4129651365067377
+	call	min_caml_atan
+#	main program ends
+	mv	a5,a0
+	mv	a0,a5
+	lw	ra,4(sp)
+	lw	s0,0(sp)
+	addi	sp,sp,8
+	jal	zero,end
+end:
+	j	end
+	.data
+L_a:	# 12.3
+	.word	1095027917
